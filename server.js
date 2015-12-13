541724273f1eefb250244cedf40036b3f310c75a
Ahmed Galal <ahmedgalal007@hotmail.com>
2015-12-13 21:48:06 +0200

    Adding POST todo Item

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 var express = require('express');
+var bodyParser = require('body-parser');
+
 var app = express();
+app.use(bodyParser.json());
+
 var PORT = process.env.PORT || 4000;
 
-var todos = [{
-	id: 1,
-	description: 'Meet mom for lunch',
-	completed: false
-},
-{
-	id: 2,
-	description: 'Go to market',
-	completed: false
-},
-{
-	id: 3,
-	description: 'Feed the cat',
-	completed: true
-}];
+var todos = [];
+var todoNextId = 1;
 
 app.get('/', function (req, res) {
 	res.send('Todo API Root');
@@ -41,6 +32,17 @@ app.get('/todos/:id', function (req, res) {
 		res.status(404).send();
 	};
 });
+
+//POST todos/
+app.post('/todos', function (req, res) {
+	var body = req.body;
+	body.id = todoNextId++;
+
+	todos.push(body);
+	
+	res.json(body);
+});
+
 
 app.listen(PORT, function () {
 	console.log('Todo API application started on port : ' + PORT);

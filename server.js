ab24b48b899bdd7ae0540d0bc38efdd697acb585
Ahmed Galal <ahmedgalal007@hotmail.com>
2015-12-18 15:42:40 +0200

    Add login route and authenticate class method

--- server.js
+++ server.js
@@ -139,7 +139,7 @@ app.post('/users/login', function(req, res) {
 	})
 })
 
-db.sequelize.sync({}).then(function() {
+db.sequelize.sync({force: true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Todo API application started on port : ' + PORT);
 	});

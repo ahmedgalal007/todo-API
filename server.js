5a6fa13072704b9a3e0fdffbea31350c5349c894
Ahmed Galal <ahmedgalal007@hotmail.com>
2015-12-19 12:11:51 +0200

    Enabling Force rebuild the database

--- server.js
+++ server.js
@@ -162,7 +162,7 @@ app.post('/users/login', function(req, res) {
 })
 
 db.sequelize.sync({
-	//force: true
+	force: true
 }).then(function() {
 	app.listen(PORT, function() {
 		console.log('Todo API application started on port : ' + PORT);

ef6b4a3f478345446b159fa3a516b47fe556f15a
Ahmed Galal <ahmedgalal007@hotmail.com>
2015-12-19 08:52:04 +0200

    Adding todo/user Assosiation

--- server.js
+++ server.js
@@ -64,8 +64,13 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
 	var body = _.pick(req.body, 'description', 'completed');
 
 	db.todo.create(body).then(function(todo) {
-		return res.status(200).json(todo.toJSON());
-	}).catch(function(e) {
+		req.user.addTodo(todo).then(function () {
+			return todo.reload()
+		}).then(function (todo) {
+			res.json(todo.toJSON());
+		})
+		//return 
+	}, function(e) {
 		res.status(400).json(e);
 	});
 });
